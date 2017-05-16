763cad1eb1240b588e33b1e84ede29210ec41908
kristine <ktomlinson@elltechnologies.com>
2017-05-16 13:04:50 -0400

    Built a simple stateless homepage component

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <h2>This is a simple note taking app</h2>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import 'babel-polyfill'; //babel can't transpile some features, so need to add this in
+import React from 'react';
+import {render} from 'react-dom';
+import HomePage from './components/home/HomePage';
+
 /* eslint-disable no-console */
 console.log('Hello World!');
+
+render(
+  <HomePage />, document.getElementById('app')
+);

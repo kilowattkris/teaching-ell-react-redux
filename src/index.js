d0865b77184ed20da1f6d28a737eb69089474ed7
kristine <ktomlinson@elltechnologies.com>
2017-05-16 16:47:49 -0400

    Added bootstrap styling as well

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App';
 import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 /* eslint-disable no-console */
 console.log('Hello World!');

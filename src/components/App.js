58845b959ffec3291af3d08c47b704a8917d2543
kristine <ktomlinson@elltechnologies.com>
2017-05-16 13:25:43 -0400

    Added App js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './home/HomePage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'babel-polyfill'; //babel can't transpile some features, so need to add this in
 import React from 'react';
 import {render} from 'react-dom';
-import HomePage from './components/home/HomePage';
+import App from './components/App';
 
 /* eslint-disable no-console */
 console.log('Hello World!');
 
 render(
-  <HomePage />, document.getElementById('app')
+  <App />, document.getElementById('app')
 );

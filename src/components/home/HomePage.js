d7c6ae620ba497b066241f0dbde8ca23300027be
kristine <ktomlinson@elltechnologies.com>
2017-05-16 16:47:01 -0400

    Added link in homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const HomePage = () => {
   return (
@@ -6,6 +7,7 @@ const HomePage = () => {
       <div>
         <h1>Welcome!</h1>
         <h3>This is a simple note taking app</h3>
+        <Link to="/notes" className="btn btn-lg btn-default">Create a note</Link>
       </div>
     </div>
   );

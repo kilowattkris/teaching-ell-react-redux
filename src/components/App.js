b2678386774b5342635f88f53ab7dff69291b799
kristine <ktomlinson@elltechnologies.com>
2017-05-16 14:01:33 -0400

    Add styling to the project

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import HomePage from './home/HomePage';
 class App extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid main-container">
         <HomePage />
       </div>
     );
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome!</h1>
-      <h2>This is a simple note taking app</h2>
+    <div className="home-page">
+      <div>
+        <h1>Welcome!</h1>
+        <h3>This is a simple note taking app</h3>
+      </div>
     </div>
   );
 };

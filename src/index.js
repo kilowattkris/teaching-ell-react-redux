5dbe6d76410ee5735d3403ba2f41788c46d8c3e1
kristine <ktomlinson@elltechnologies.com>
2017-05-19 19:00:52 -0400

    Implemented the store

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import Root from './routes';
 import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {initialState} from './reducers/initialState';
 
 /* eslint-disable no-console */
 console.log('Hello World!');
 
+const store = configureStore(initialState);
+
 render(
-  <Root />, document.getElementById('app')
+  <Provider store={store}>
+    <Root />
+  </Provider>, document.getElementById('app')
 );
---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'; //enforces immutability
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}

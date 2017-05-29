b4959514896eaba0cd15acd6f22abd58d5d5763f
kristine <ktomlinson@elltechnologies.com>
2017-05-29 15:15:10 -0400

    fixed something that didn't get updated

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -7,6 +7,6 @@ export default function noteReducer(state = initialState.notes, action) {
       return [...state, Object.assign({}, action.note)];
 
     default:
-      return initialState.notes;
+      return state;
   }
 }
